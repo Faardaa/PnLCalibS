d456ad537d7b15107a8b06ccc4bc80bc5e8d75c0
Faardaa <136010128+Faardaa@users.noreply.github.com>
2026-03-29 01:25:19 +0400

    Reduce OUTLIER_THRESHOLD from 0.05 to 0.01

--- inference.py
+++ inference.py
@@ -18,7 +18,7 @@ from utils.utils_calib import FramebyFrameCalib, pan_tilt_roll_to_orientation
 from utils.utils_heatmap import get_keypoints_from_heatmap_batch_maxpool, get_keypoints_from_heatmap_batch_maxpool_l, \
     complete_keypoints, coords_to_dict
 
-OUTLIER_THRESHOLD = 0.05 
+OUTLIER_THRESHOLD = 0.01 
 
 
 lines_coords = [[[0., 54.16, 0.], [16.5, 54.16, 0.]],
